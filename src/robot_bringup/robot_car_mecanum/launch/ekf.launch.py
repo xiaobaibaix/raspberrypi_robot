6b0b92a348eeab41456a0011b07002e33e458d8a
xiaobaibaix <2308043842@qq.com>
2025-11-26 18:03:08 +0800

    add ekf lanucn

---
+++ src/robot_bringup/robot_car_mecanum/launch/ekf.launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('robot_car_mecanum')
+    
+    return LaunchDescription([
+        # EKF融合节点
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[os.path.join(pkg_share, 'config', 'ekf.yaml')],
+            remappings=[
+                ('/odometry/filtered', '/odom'),
+                ('/accel/filtered', '/accel')
+            ]
+        ),
+    ])
