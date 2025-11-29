ddbc7ca53cb41e859885539a9acdec2d011ff2e5
xiaobaibaix <2308043842@qq.com>
2025-11-29 17:37:42 +0800

    add model slam nav2 sim

---
+++ src/slam_cartographer/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'slam_cartographer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+'/config', glob("config/*.lua")),
+        ('share/' + package_name+'/launch', glob("launch/*.launch.py")),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='huazhi',
+    maintainer_email='2308043842@qq.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ src/slam_toolboxs/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'slam_toolboxs'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='huazhi',
+    maintainer_email='2308043842@qq.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
